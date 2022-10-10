adcad5d8a19ddb0d8bbed248eca513770ab24d9e
phenic00 <otworip@gmail.com>
2022-10-10 13:18:25 +0300

    Added user prompts

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function akanNames() {
+    var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    // WE DECLARE AND INITIALISE AN ARRAY WITH THE DAYS OF THE WEEK STARTING FROM SUNDAY TO MONDAY
+
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    //ARRAY OF FEMALE AKAN NAMES
+
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+    //ARRAY OF MALE AKAN NAMES
+    // user prompts
+
+    var dateInput = prompt("Enter date of birth(YYYY-MM-DD):");//prompts user input
+
+    var birthDates = new Date(dateInput);//uses variable dataIput from user prompt
+
+    var dateExact = birthDates.getDay();
+
+    var birthMonth = birthDates.getMonth();//PICKS the month
+
+    var birthYear = birthDates.getFullYear();// picks the year
+}
